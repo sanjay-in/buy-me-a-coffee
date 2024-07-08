e9622dd66c7f900ca3f9ff2c6ba18d019ed0b862
sanjay-in <sanjaysridharan@outlook.com>
2024-07-08 20:06:45 +0530

    Add buyCoffee function

--- contracts/BuyCoffee.sol
+++ contracts/BuyCoffee.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.8;
 
+error BuyCoffee__NotEnoughMoneyTBuyCoffee();
+
 contract BuyCoffee {
     struct Memo {
         address from;
@@ -19,9 +21,35 @@ contract BuyCoffee {
     );
 
     address payable owner;
-    Memo[] public memo;
+    Memo[] public memos;
 
     constructor() {
         owner = payable(msg.sender);
+    }
+
+    /**
+     * Buys coffee for the owner
+     * @param _name name of the purchaser of coffee
+     * @param _message message from the purchaser
+     */
+    function buyCoffee(
+        string memory _name,
+        string memory _message
+    ) public payable {
+        if (msg.value <= 0) {
+            revert BuyCoffee__NotEnoughMoneyTBuyCoffee();
+        }
+
+        memos.push(
+            Memo(msg.sender, _name, _message, msg.value, block.timestamp)
+        );
+
+        emit CoffeeBought(
+            msg.sender,
+            _name,
+            _message,
+            msg.value,
+            block.timestamp
+        );
     }
 }
