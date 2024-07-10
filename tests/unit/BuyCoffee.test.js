f7e0ae92cc0f57affbdb7742cd72ff8bf8e64629
sanjay-in <sanjaysridharan@outlook.com>
2024-07-10 22:39:41 +0530

    update unit test

--- tests/unit/BuyCoffee.test.js
+++ tests/unit/BuyCoffee.test.js
@@ -1,23 +1,26 @@
 const { assert, expect } = require("chai");
 const { network, deployments, ethers, getNamedAccounts } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
 
-describe("BuyCoffee", () => {
-  let buyCoffee;
-  let deployer;
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("BuyCoffee", () => {
+      let buyCoffee;
+      let deployer;
 
-  beforeEach(async () => {
-    deployer = (await getNamedAccounts()).deployer;
-    await deployments.fixture(["all"]);
+      beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer;
+        await deployments.fixture(["all"]);
 
-    buyCoffee = await ethers.getContract("BuyCoffee", deployer);
-  });
+        buyCoffee = await ethers.getContract("BuyCoffee", deployer);
+      });
 
-  describe("constructor", () => {
-    it("sets the deployer correctly", async () => {
-      const ownerAddress = await buyCoffee.i_owner;
-      console.log("ownerAddress", ownerAddress);
-      console.log("deployer", deployer);
-      assert.equal(ownerAddress, deployer);
+      describe("constructor", () => {
+        it("sets the deployer correctly", async () => {
+          const ownerAddress = await buyCoffee.i_owner;
+          console.log("ownerAddress", ownerAddress);
+          console.log("deployer", deployer);
+          assert.equal(ownerAddress, deployer);
+        });
+      });
     });
-  });
-});
