e34529e601143c1242d8a5eb611db28e6258f231
sanjay-in <sanjaysridharan@outlook.com>
2024-07-10 22:10:57 +0530

    add unit test

---
+++ tests/unit/BuyCoffee.test.js
@@ -0,0 +1,23 @@
+const { assert, expect } = require("chai");
+const { network, deployments, ethers, getNamedAccounts } = require("hardhat");
+
+describe("BuyCoffee", () => {
+  let buyCoffee;
+  let deployer;
+
+  beforeEach(async () => {
+    deployer = (await getNamedAccounts()).deployer;
+    await deployments.fixture(["all"]);
+
+    buyCoffee = await ethers.getContract("BuyCoffee", deployer);
+  });
+
+  describe("constructor", () => {
+    it("sets the deployer correctly", async () => {
+      const ownerAddress = await buyCoffee.i_owner;
+      console.log("ownerAddress", ownerAddress);
+      console.log("deployer", deployer);
+      assert.equal(ownerAddress, deployer);
+    });
+  });
+});
