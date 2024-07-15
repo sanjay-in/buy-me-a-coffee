bbcc5e80f15d679b2c78a0c0a8c5c5e2ee985e7b
sanjay-in <sanjaysridharan@outlook.com>
2024-07-15 09:24:05 +0530

    update scripts

--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -3,8 +3,21 @@ const hre = require("hardhat");
 const printBalances = async (addresses) => {
   for (let i = 0; i < addresses.length; i++) {
     const balance = await hre.ethers.provider.getBalance(addresses[i]);
+    const formattedEther = await ethers.utils.formatEther(balance);
+    console.log(`Balance for Account ${i}: ${formattedEther}`);
+  }
+};
+
+const printMemos = (memos) => {
+  for (let memo of memos) {
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+    const amount = hre.ethers.utils.formatEther(memo.amount);
+    // converts the timestamp to milliseconds (by multiplying with 1000) and create a new Date object in human readable date
+    const timestamp = new Date(memo.timestamp.toNumber() * 1000).toDateString();
     console.log(
-      `Balance for Account ${i}: ${ethers.utils.formatEther(balance)}`
+      `${name} gave ${amount} ETH on ${timestamp} from address ${from} and said: "${message}"`
     );
   }
 };
@@ -25,21 +38,34 @@ const main = async () => {
     buyCoffee.address,
   ];
   console.log("----- Balance before funding -----");
-  printBalances(addresses);
+  await printBalances(addresses);
 
   // Calling buyCoffee function from funders account
+  const { parseEther } = hre.ethers.utils;
+
   await buyCoffee
     .connect(funder1)
-    .buyCoffee("A", "First message", { value: ethers.utils.parseEther("0.1") });
-  await buyCoffee.connect(funder2).buyCoffee("B", "First message", {
-    value: ethers.utils.parseEther("0.005"),
-  });
+    .buyCoffee("A", "First message", { value: parseEther("0.1") });
+  await buyCoffee
+    .connect(funder2)
+    .buyCoffee("B", "Second message", { value: parseEther("0.005") });
   await buyCoffee
     .connect(funder3)
-    .buyCoffee("C", "First message", { value: ethers.utils.parseEther("1") });
+    .buyCoffee("C", "Third message", { value: parseEther("1") });
 
   console.log("----- Balance after funding -----");
-  printBalances(addresses);
+  await printBalances(addresses);
+
+  // Withdraw
+  await buyCoffee.connect(deployer).withdraw();
+
+  console.log("----- Balance after withdrawn -----");
+  await printBalances(addresses);
+
+  // Get memos
+  console.log("----- Memos -----");
+  const memos = await buyCoffee.getAllMemos();
+  printMemos(memos);
 };
 
 main()
