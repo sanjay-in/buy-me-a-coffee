64d905b9979935102c7e1936f2f2de63ae5e29ee
sanjay-in <sanjaysridharan@outlook.com>
2024-07-17 03:10:05 +0530

    Add modal

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 require("solidity-coverage");
 require("hardhat-deploy");
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_ALCHEMY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
@@ -12,9 +16,24 @@ module.exports = {
     hardhat: {
       chainId: 31337,
     },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      blockConfirmations: 6,
+    },
   },
   solidity: {
     compilers: [{ version: "0.8.24" }],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
   },
   namedAccounts: {
     deployer: {
